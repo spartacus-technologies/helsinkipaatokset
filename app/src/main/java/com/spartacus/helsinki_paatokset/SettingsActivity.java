a79e7c27a9563ac91d30385c5be9d9f615c6906a
eetu <eetu.prehti@gmail.com>
2015-10-27 19:13:48 +0200

    Added ConfigurationManager for saving settings.

--- app/src/main/java/com/spartacus/helsinki_paatokset/SettingsActivity.java
+++ app/src/main/java/com/spartacus/helsinki_paatokset/SettingsActivity.java
@@ -2,11 +2,13 @@ package com.spartacus.helsinki_paatokset;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+
+import com.spartacus.helsinki_paatokset.data_access.ConfigurationManager;
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,10 +18,32 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
         setContentView(R.layout.activity_settings);
 
         //Set some default values
-        ((TextView)findViewById(R.id.textViewVersionInfoAcitivitySettings)).setText(BuildConfig.VERSION_NAME + " (build #" + BuildConfig.VERSION_CODE+ ")");
+        ((TextView)findViewById(R.id.textViewVersionInfoAcitivitySettings)).setText(BuildConfig.VERSION_NAME + " (build #" + BuildConfig.VERSION_CODE + ")");
+
+        //Radio button group selection:
+        ConfigurationManager.initialize(this);
+        String video_source =  ConfigurationManager.getVideoSource();
+
+        if(video_source.equals("MP4")){
+
+            ((RadioButton)findViewById(R.id.radioButtonMP4)).setChecked(true);
+        }
+        else if(video_source.equals("OGV")){
+
+            ((RadioButton)findViewById(R.id.radioButtonOGV)).setChecked(true);
+        }
+        else if(video_source.equals("HKI")){
+
+            ((RadioButton)findViewById(R.id.radioButtonHKIKanava)).setChecked(true);
+        }
+
 
         //Listeners
         findViewById(R.id.buttonCloseSettingsActivity).setOnClickListener(this);
+
+        ((RadioButton)findViewById(R.id.radioButtonMP4)).setOnClickListener(this);
+        ((RadioButton)findViewById(R.id.radioButtonHKIKanava)).setOnClickListener(this);
+        ((RadioButton)findViewById(R.id.radioButtonOGV)).setOnClickListener(this);
     }
 
     @Override
@@ -47,6 +71,27 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
     @Override
     public void onClick(View v) {
 
-        super.finish();
+        switch (v.getId()){
+
+            case R.id.radioButtonMP4:
+
+                ConfigurationManager.setVideoSource("MP4");
+                break;
+
+            case R.id.radioButtonHKIKanava:
+
+                ConfigurationManager.setVideoSource("HKI");
+                break;
+
+            case R.id.radioButtonOGV:
+
+                ConfigurationManager.setVideoSource("OGV");
+                break;
+
+            case R.id.buttonCloseSettingsActivity:
+
+                super.finish();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/spartacus/helsinki_paatokset/data_access/ConfigurationManager.java
@@ -0,0 +1,50 @@
+package com.spartacus.helsinki_paatokset.data_access;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Eetu on 27.10.2015.
+ */
+public class ConfigurationManager {
+
+    private static final String settings_path = "HKIPaatokset";
+
+    static SharedPreferences pref = null;
+    static SharedPreferences.Editor editor = null;
+
+
+
+    public static void initialize(Context context){
+
+        pref = context.getSharedPreferences(settings_path, Context.MODE_PRIVATE);
+        editor = pref.edit();
+    }
+
+    //Setters
+    //=======
+
+    //Possible values for video source:
+    // 'MP4'
+    // 'OGV'
+    // 'HKI'
+    public static void setVideoSource(String source){
+
+        editor.putString("VideoMode", source);
+        editor.commit();
+    }
+
+
+
+    //Getters
+    //=======
+
+    // 'MP4'
+    // 'OGV'
+    // 'HKI'
+    public static String getVideoSource(){
+
+        return pref.getString("VideoMode", "MP4");
+    }
+
+}
